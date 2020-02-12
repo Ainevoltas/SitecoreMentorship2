30c6db9a897992dd6b04ebb62014289f497033a4
Ainevoltas <gus_1325@hotmail.com>
2020-02-12 04:23:33 +0000

    Added basic DI test example into the code

---
+++ events.tac.local/GenericCalloutServicesConfigurator.cs
@@ -0,0 +1,18 @@
+﻿using events.tac.local.Controllers;
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace events.tac.local
+{
+    public class GenericCalloutServicesConfigurator : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddTransient(typeof(GenericCalloutController));
+        }
+    }
+}
